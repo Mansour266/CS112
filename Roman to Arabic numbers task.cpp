efdf6e0ed1a4bb07d33c3e408e1a1fc966d6012e
Mansour266 <mansour24@protonmail.com>
2022-03-14 20:29:11 +0200

    After understanding the assignment right

--- Roman to Arabic numbers task.cpp
+++ Roman to Arabic numbers task.cpp
@@ -9,13 +9,10 @@
 
 using namespace std;
 
-int roman_to_arabic(){
+int main(){
     string roman_num;
-    int arabic_number {0};
-    // this should be the converted number at the end
-    cout << "Enter roman number: ";
+    int arabic_number {0}; // this should be the converted number at the end
     cin >> roman_num;
-    
     while (true){
         if(roman_num[0] == 'M'){
             arabic_number += 1000;
@@ -36,7 +33,7 @@ int roman_to_arabic(){
                 arabic_number -= 100;
             }
             else{
-                arabic_number += 100;
+           arabic_number += 100;
             }
             roman_num.erase(0,1);
         }
@@ -80,70 +77,5 @@ int roman_to_arabic(){
             break;
         }
     }
-    cout << arabic_number << endl;
-
-    return arabic_number;
-}
-
-string arabic_to_roman(){
-    int arabic_number;
-    string roman_number;
-
-    cout << "Enter arabic number: ";
-    cin >> arabic_number;
-
-    while (true) {
-        if (arabic_number >= 1000) {
-            roman_number += 'M';
-            arabic_number -= 1000;
-        } else if (arabic_number >= 500) {
-            roman_number += 'D';
-            arabic_number -= 500;
-        } else if (arabic_number >= 100) {
-            roman_number += 'C';
-            arabic_number -= 100;
-        } else if (arabic_number >= 50) {
-            roman_number += 'L';
-            arabic_number -= 50;
-        } else if (arabic_number >= 10) {
-            roman_number += 'X';
-            arabic_number -= 10;
-        } else if (arabic_number >= 5) {
-            roman_number += 'V';
-            arabic_number -= 5;
-        } else if (arabic_number >= 1) {
-            roman_number += "I";
-            arabic_number -= 1;
-        } else if(arabic_number == 0){
-            break;
-        }
-    }
-    cout << roman_number << endl;
-    return roman_number;
-    // there is a better way to do this but the syntax and data structure are too hard for me but this method is correct
-}
-int choices(){ // this should loop through the choices and display them until the user chooses 3 to exit
-    int choice = 0;
-
-    while(choice != 3){
-        cout << "Enter 1 to convert from roman to arabic" << endl;
-        cout << "Enter 2 to convert from arabic to roman" << endl;
-        cout << "Enter 3 to exit: ";
-        cin >> choice;
-
-        if(choice == 1){
-            roman_to_arabic();
-        }
-
-        else if(choice == 2){
-            arabic_to_roman();
-        }
-    }
-
-    return 0;
-}
-int main(){
-
-    choices();
-
+    cout << arabic_number;
 }
